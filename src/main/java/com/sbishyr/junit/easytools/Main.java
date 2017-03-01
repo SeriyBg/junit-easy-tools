24bb313e2279185471e9b3d99d4c41d639664a29
Serge Bishyr <seriybg@gmail.com>
2017-03-01 10:29:49 +0200

    Initial commit

---
+++ src/main/java/com/sbishyr/junit/easytools/Main.java
@@ -0,0 +1,8 @@
+package com.sbishyr.junit.easytools;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/test/java/com/sbishyr/junit/easytools/MainTest.java
@@ -0,0 +1,11 @@
+package com.sbishyr.junit.easytools;
+
+import org.junit.Test;
+
+public class MainTest {
+
+    @Test
+    public void initialTest() throws Exception {
+        System.out.println("Pass");
+    }
+}
