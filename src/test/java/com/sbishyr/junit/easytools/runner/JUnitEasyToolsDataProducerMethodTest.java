131bf918578cf23ba5e086e0ebe913623d6522d2
Serge Bishyr <seriybg@gmail.com>
2017-04-02 19:48:24 +0300

    #29 - add test
    - test is not failing because of #33. Fix it before continue

---
+++ src/test/java/com/sbishyr/junit/easytools/runner/JUnitEasyToolsDataProducerMethodTest.java
@@ -0,0 +1,42 @@
+package com.sbishyr.junit.easytools.runner;
+
+import com.sbishyr.junit.easytools.model.annotation.DataProducer;
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.RunWith;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Supplier;
+
+import static com.sbishyr.junit.easytools.utils.ResultAssertions.assertResultHasNoFailures;
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author Serge Bishyr
+ */
+public class JUnitEasyToolsDataProducerMethodTest {
+
+    @RunWith(JUnitEasyTools.class)
+    public static class MethodAsDataProducer {
+
+        @DataProducer
+        public static List<Supplier<String>> dataProducer() {
+            return Arrays.asList(
+                    () -> "return"
+            );
+        }
+
+        @Test
+        public void a(String s) {
+            assertThat(s).isEqualTo("return");
+        }
+    }
+
+    @Test
+    public void shouldSupportMethodWithCollectionReturnAsDataProducer() throws Exception {
+        Result result = JUnitCore.runClasses(MethodAsDataProducer.class);
+        assertResultHasNoFailures(result);
+    }
+}
