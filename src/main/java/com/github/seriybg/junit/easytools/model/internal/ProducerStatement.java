a46c7d5b405d1be8edfbce38485e9f9c50991f3b
Serge Bishyr <seriybg@gmail.com>
2017-06-10 13:22:20 +0300

    close #34 add assumption support

--- src/main/java/com/github/seriybg/junit/easytools/model/internal/ProducerStatement.java
+++ src/main/java/com/github/seriybg/junit/easytools/model/internal/ProducerStatement.java
@@ -6,6 +6,11 @@ import org.junit.runners.model.FrameworkMethod;
 import org.junit.runners.model.Statement;
 import org.junit.runners.model.TestClass;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.fail;
+
 /**
  * @author Serge Bishyr
  */
@@ -13,6 +18,8 @@ public class ProducerStatement extends Statement {
 
     private final TestClass testClass;
     private final FrameworkMethod method;
+    private int success = 0;
+    private final List<AssumptionViolatedException> failedAssumptions = new ArrayList<>();
 
     public ProducerStatement(TestClass testClass, FrameworkMethod method) {
         this.testClass = testClass;
@@ -27,9 +34,14 @@ public class ProducerStatement extends Statement {
         for (int i = 0; i < iterations; i++) {
             try {
                 new AssignmentsStatement(testClass.getJavaClass(), method, assignments, i).evaluate();
+                success++;
             } catch (AssumptionViolatedException e) {
-                //ignore for now
+                failedAssumptions.add(e);
             }
+        }
+        if (success == 0) {
+            fail("Never found parameters that satisfied method assumptions.  Violated assumptions: "
+                    + failedAssumptions);
         }
     }
 }
