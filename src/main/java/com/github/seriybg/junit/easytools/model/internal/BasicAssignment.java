610d2386bafdea5a3efc4ade8d7dc6ea8357553a
Serge Bishyr <seriybg@gmail.com>
2017-06-11 14:11:14 +0300

    Fix #52 add supporting of unboxing @DataProducers to primitive types

--- src/main/java/com/github/seriybg/junit/easytools/model/internal/BasicAssignment.java
+++ src/main/java/com/github/seriybg/junit/easytools/model/internal/BasicAssignment.java
@@ -1,18 +1,36 @@
 package com.github.seriybg.junit.easytools.model.internal;
 
-import com.github.seriybg.junit.easytools.model.annotation.ProducedValue;
 import com.github.seriybg.junit.easytools.model.annotation.DataProducer;
+import com.github.seriybg.junit.easytools.model.annotation.ProducedValue;
 import org.junit.experimental.theories.ParameterSignature;
 
 import java.lang.reflect.AccessibleObject;
 import java.lang.reflect.Member;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author Serge Bishyr
  */
 abstract class BasicAssignment<T extends AccessibleObject & Member> implements Assignment {
+
+    private static final Map<Type, Class<?>> primitiveSupplierToType = createPrimitiveMappings();
+
+    private static Map<Type,Class<?>> createPrimitiveMappings() {
+        Map<Type, Class<?>> map = new HashMap<>();
+        map.put(Integer.class, Integer.TYPE);
+        map.put(Long.class, Long.TYPE);
+        map.put(Byte.class, Byte.TYPE);
+        map.put(Boolean.class, Boolean.TYPE);
+        map.put(Short.class, Short.TYPE);
+        map.put(Character.class, Character.TYPE);
+        map.put(Double.class, Double.TYPE);
+        map.put(Float.class, Float.TYPE);
+        return Collections.unmodifiableMap(map);
+    }
 
     private final T assignmentProducer;
 
@@ -36,7 +54,8 @@ abstract class BasicAssignment<T extends AccessibleObject & Member> implements A
             ParameterizedType parametrizedType = (ParameterizedType) genericType;
             Type[] actualTypeArguments = parametrizedType.getActualTypeArguments();
             Type actualTypeArgument = actualTypeArguments[0];
-            if (parameterSignature.getType().equals(actualTypeArgument)) {
+            if (parameterSignature.getType().equals(actualTypeArgument)
+                    || parameterSignature.getType().equals(primitiveSupplierToType.get(actualTypeArgument))) {
                 return true;
             }
         }
--- src/test/java/com/github/seriybg/junit/easytools/runner/JUnitEasyToolsTypesTest.java
+++ src/test/java/com/github/seriybg/junit/easytools/runner/JUnitEasyToolsTypesTest.java
@@ -7,6 +7,7 @@ import org.junit.runner.RunWith;
 import java.util.function.Supplier;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 /**
  * @author Serge Bishyr
@@ -17,8 +18,59 @@ public class JUnitEasyToolsTypesTest {
     @DataProducer
     public static Supplier<Integer> intsSupplier = () -> 227;
 
+    @DataProducer
+    public static Supplier<Long> longSupplier = () -> 1234L;
+
+    @DataProducer
+    public static Supplier<Byte> byteSupplier = () -> (byte) 27;
+
+    @DataProducer
+    public static Supplier<Boolean> booleanSupplier = () -> false;
+
+    @DataProducer
+    public static Supplier<Short> shortSupplier = () -> (short) 15;
+
+    @DataProducer
+    public static Supplier<Character> characterSupplier = () -> 's';
+
+    @DataProducer
+    public static Supplier<Double> doubleSupplier = () -> 2.7;
+
+    @DataProducer
+    public static Supplier<Float> floatSupplier = () -> 2.7f;
+
     @Test
     public void shouldSupplyIntsFromInteger(int value) throws Exception {
         assertEquals(value, 227);
+    }
+
+    @Test
+    public void shouldSupplyLongTypeFromLong(long value) throws Exception {
+        assertEquals(value, 1234L);
+    }
+
+    @Test
+    public void shouldSupplyByteTypeFromByte(byte value) throws Exception {
+        assertEquals(value, (byte) 27);
+    }
+
+    @Test
+    public void shouldSupplyBoolTypeFromBoolean(boolean value) throws Exception {
+        assertFalse(value);
+    }
+
+    @Test
+    public void shouldSupplyShortTypeFromShort(short value) throws Exception {
+        assertEquals(value, (short) 15);
+    }
+
+    @Test
+    public void shouldSupplyCharTypeFromCharacter(char value) throws Exception {
+        assertEquals(value, 's');
+    }
+
+    @Test
+    public void shouldSupplyFloatTypeFromFloat(float value) throws Exception {
+        assertEquals(value, 2.7f, 2.7);
     }
 }
